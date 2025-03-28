f28b76ab5ec74a759562fedc737e50e76113bd83
Federico Ariel Aguirre <64861254+FedericoArielAguirre@users.noreply.github.com>
2025-03-28 15:27:17 -0300

    Update TP2EJ11.m
    Fixed some spaces

--- TP2EJ11.m
+++ TP2EJ11.m
@@ -17,7 +17,7 @@ x = 1 ./ (c.^2); % x = 1 / c^2
 p = polyfit(x, y, 1); % p(1) = -c_s, p(2) = k_max
 
 % Coeficientes
-c_s = -p(1);
+c_s   = -p(1);
 k_max = p(2);
 
 fprintf('Estimaciones:\n');
@@ -25,7 +25,7 @@ fprintf('c_s = %.4f mg/L\n', c_s);
 fprintf('k_max = %.4f\n', k_max);
 
 % Pronóstico para c = 2 mg/L
-c_new = 2; % Nueva concentración
+c_new  = 2; % Nueva concentración
 k_pred = (k_max * c_new^2) / (c_s + c_new^2); % Usar la ecuación original
 
 fprintf('Pronóstico de la tasa de crecimiento para c = %.2f mg/L: k = %.4f\n', c_new, k_pred);
